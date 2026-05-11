36fe3e6e3fefbb54b37b098e3c1813eadc3cdd7e
nikanor-148 <nikitka.romanov.999@gmail.com>
2026-05-11 13:15:08 +0400

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main{
+    public static void main(String[] args) {
+        User u = new User();
+        User k = new User();
+        u.name = "Nikita";
+        u.age = 26;
+        k.name = "Vlad";
+        k.age = 31;
+        u.sayHello();
+        k.sayHello();
+    }
+}
+//
---
+++ src/User.java
@@ -0,0 +1,10 @@
+public class User {
+    String name;
+    int age;
+
+    void    sayHello(){
+        System.out.println("Hi,i'm " + name);
+    }
+}
+
+
